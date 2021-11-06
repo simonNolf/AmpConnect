8d2b26a2d600bfa58b9bfcdd962710ebccc929cd
simonNolf <63592563+simonNolf@users.noreply.github.com>
2021-11-06 18:32:02 +0100

    fetch music

---
+++ appli/app/src/main/java/com/example/ampconnect/Modes/SongModel.kt
@@ -0,0 +1,13 @@
+package com.example.ampconnect.Modes
+
+import android.net.Uri
+
+class SongModel (
+    var song_id: Long,
+    var song_tittle:String,
+    var artist : String,
+    var songData: String,
+    var date : Long,
+    var image : Uri
+        ){
+}
