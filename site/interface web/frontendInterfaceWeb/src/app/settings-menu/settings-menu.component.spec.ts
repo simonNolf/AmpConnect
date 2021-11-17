21901cd4d351c66b0ecc52aa9a74294945bae294
NathanGregoar <he201832@students.ephec.be>
2021-11-17 20:39:31 +0100

    Ajouts de tests unitaires
    Ajouts du début des tests unitaires

--- site/interface web/frontendInterfaceWeb/src/app/settings-menu/settings-menu.component.spec.ts
+++ site/interface web/frontendInterfaceWeb/src/app/settings-menu/settings-menu.component.spec.ts
@@ -3,5 +3,5 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SettingsMenuComponent } from './settings-menu.component';
 
 describe('SettingsMenuComponent', () => {
-
+    
 });
