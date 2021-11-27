466775b88c6853e3f049cc6aaad2398292f702ef
Valentin <valentinderreumaux03@gmail.com>
2021-11-27 16:13:56 +0100

    update icon
    update des icon + rename files

--- appli/app/src/main/java/com/example/youtubeapikotlin/ui/notifications/NotificationsFragment.kt
+++ appli/app/src/main/java/com/example/youtubeapikotlin/ui/notifications/NotificationsFragment.kt
@@ -9,12 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.youtubeapikotlin.R
-import com.example.youtubeapikotlin.databinding.FragmentNotificationsBinding
+import com.example.youtubeapikotlin.databinding.FragmentLocalBinding
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentLocalBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,7 +28,7 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentLocalBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textNotifications
