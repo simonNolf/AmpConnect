ee946e19b9bc03cbc4047c787ddfa9bc0f51a368
WassimBouhdid <HE201893@students.ephec.be>
2021-11-30 21:05:17 +0100

    Upload de fichier
    Nous pouvons maintenant sélectionner un fichier et l'envoyer vers le server nodeJS

--- site/interface web/frontendInterfaceWeb/src/app/app.module.ts
+++ site/interface web/frontendInterfaceWeb/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SettingsMenuComponent } from './settings-menu/settings-menu.component';
@@ -39,6 +41,7 @@ const appRoutes: Routes = [
     
   ],
   imports: [
+    ReactiveFormsModule,
     FormsModule,
     BrowserModule,
     AppRoutingModule,
