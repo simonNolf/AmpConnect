fed93d37ddd7f8013e38491ac57fc61243ecf485
saskia-libotte <s.libotte@students.ephec.be>
2021-12-01 10:42:42 +0100

    home page
    refactor  de la page d'accueil avec ajout de minuteur

--- site/site vitrine/Frontend/src/app/pages/home/home.component.ts
+++ site/site vitrine/Frontend/src/app/pages/home/home.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  title = "Give A Seconde Life To Your Old Speakers";
+  title2 ="We're";
+  title3 = "Launching";
+  title4 = "Soon"
+  //les variables du countDown
+  days = 0;
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
 
-  constructor() { }
+  constructor() {
+   }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    this.countDown;
   }
 
+  //Creative Developer -- chaine youtube tuto --
+  countDown = setInterval(()=>{
+      const countDate = new Date("Dec 23, 2021 12:00:00").getTime();
+      const currentDate = new Date().getTime();
+      var gap = countDate - currentDate;
+
+      // variables de cacules
+      const seconds = 1000;
+      const minutes = seconds * 60;
+      const hours = minutes * 60;
+      const days = hours*24;
+
+      this.days = Math.floor(gap / (days));
+      this.hours = Math.floor((gap % (days)) / (hours));
+      this.minutes = Math.floor((gap % (hours)) / (minutes));
+      this.seconds = Math.floor((gap % (minutes)) / (seconds));
+  })
 }
