26e3a42c7053abeb1a37378b5ab2c3230a01bb7f
simonNolf <63592563+simonNolf@users.noreply.github.com>
2021-12-18 13:46:37 +0100

    boutton qui change play/pause

--- appli/app/src/main/java/com/example/youtubeapikotlin/ui/local/LocalFragment.java
+++ appli/app/src/main/java/com/example/youtubeapikotlin/ui/local/LocalFragment.java
@@ -3,6 +3,7 @@ package com.example.youtubeapikotlin.ui.local;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,30 +11,68 @@ import android.widget.Button;
 import android.widget.SeekBar;
 
 import androidx.fragment.app.Fragment;
+
 import com.example.youtubeapikotlin.R;
 
 public class LocalFragment extends Fragment {
 
     Button play;
     SeekBar seekBar;
+    Runnable runnable;
+    Handler handler;
+    MediaPlayer mediaplayer;
+    int images[] = {R.drawable.play, R.drawable.pause};
+    int i = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_local, container, false);
 
-    play = (Button) rootView.findViewById(R.id.playBtn);
-    seekBar = rootView.findViewById(R.id.positionBar);
+        play = (Button) rootView.findViewById(R.id.playBtn);
+        seekBar = rootView.findViewById(R.id.positionBar);
+        handler = new Handler();
+        mediaplayer = new MediaPlayer();
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    mediaplayer.seekTo(progress);
+                    seekBar.setProgress(progress);
+                }
+            }
 
-    final MediaPlayer sound = MediaPlayer.create(getActivity(), R.raw.song);
-    play.setOnClickListener(new View.OnClickListener(){
-        @Override
-        public void onClick(View v){
-            sound.setLooping(true);
-            sound.start();
-        }
-    });
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
-    return rootView;
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+
+        final MediaPlayer sound = MediaPlayer.create(getActivity(), R.raw.song);
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                seekBar.setMax(sound.getDuration());
+                sound.setLooping(true);
+                sound.start();
+                play.setBackgroundResource(images[i]);
+                i++;
+                if(i==2) {
+                    i = 0;
+                }
+
+
+
+            }
+        });
+
+
+        return rootView;
 
     }
 
